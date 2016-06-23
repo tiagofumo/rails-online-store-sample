4d00135b6e909fe4391097c8d6ebdd518bbea849
tiagofumo <tiagofumo@gmail.com>
2016-06-23 00:47:41 -0300

    basic layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :load_user
+
+  def load_user
+    if user_signed_in? then
+      @user = current_user
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :trackable, :validatable, :confirmable
+
+  def first_name
+    name.split.first
+  end
+
+  def last_name
+    name.split.last
+  end
 end
