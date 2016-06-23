ba87bebe830fdac925a2631c3cd4a92d3a7888e5
tiagofumo <tiagofumo@gmail.com>
2016-06-23 05:38:34 -0300

    adding profile picture to the user (not complete)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622041031) do
+ActiveRecord::Schema.define(version: 20160623052834) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20160622041031) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
