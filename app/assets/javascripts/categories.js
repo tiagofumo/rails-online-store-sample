0760f47af8de699d938e9e0f85ab5dd19dd0f033
tiagofumo <tiagofumo@gmail.com>
2016-07-19 01:06:34 -0300

    fix arrow not working bug

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,4 +1,4 @@
-$(document).on('ready page:change', function() {
+$(document).on('page:change', function() {
   $('.category_arrow').click(function() {
     $(this).siblings('.category_list').toggle();
   });
