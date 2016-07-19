0cdc13ea6213e7f2642bb85398ef1135d856daf8
tiagofumo <tiagofumo@gmail.com>
2016-07-19 20:40:58 -0300

    add cart count on page header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :load_user, :load_categories
+  before_action :load_user, :load_categories, :load_cart_size
 
   private
     def load_user
@@ -13,5 +13,13 @@ class ApplicationController < ActionController::Base
 
     def load_categories
       @categories = Category.all.order :name
+    end
+
+    def load_cart_size
+      if user_signed_in?
+        @cart_size = CartItem.where(user_id: @current_user).sum(:quantity)
+      else
+        @cart_size = 0
+      end
     end
 end
