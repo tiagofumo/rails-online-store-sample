200e76b92f72e5b5bdf9560ba85650e4421db0d0
tiagofumo <tiagofumo@gmail.com>
2016-07-22 02:14:13 -0300

    add new products carousel on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    @products = Product.includes(:primary_picture).
+                        order(created_at: :desc).
+                        limit(24)
   end
 
   def about
