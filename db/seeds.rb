c90150d7349667a38aebcdf384612d8014d545e2
tiagofumo <tiagofumo@gmail.com>
2016-07-24 14:50:10 -0300

    add seeds for shipping methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ShippingMethod.create name: 'Local Pickup', base_cost: 0,
+                      extra_cost_per_gram: 0, base_cost_max_weight: 0,
+                      delivery_time_min: 0, delivery_time_max: 0
+
+ShippingMethod.create name: 'USPS First-Class', base_cost: 10,
+                      extra_cost_per_gram: 0.10, base_cost_max_weight: 250,
+                      delivery_time_min: 5, delivery_time_max: 8
+
+ShippingMethod.create name: 'USPS Priority', base_cost: 15,
+                      extra_cost_per_gram: 0.15, base_cost_max_weight: 250,
+                      delivery_time_min: 3, delivery_time_max: 5
+
+ShippingMethod.create name: 'UPS Ground', base_cost: 12,
+                      extra_cost_per_gram: 0.15, base_cost_max_weight: 200,
+                      delivery_time_min: 5, delivery_time_max: 8
+
+ShippingMethod.create name: 'UPS 3 Day Select', base_cost: 20,
+                      extra_cost_per_gram: 0.2, base_cost_max_weight: 200,
+                      delivery_time_min: 3, delivery_time_max: 3
+
+ShippingMethod.create name: 'Fedex', base_cost: 45, extra_cost_per_gram: 0.5,
+                      base_cost_max_weight: 250, delivery_time_min: 2,
+                      delivery_time_max: 4
