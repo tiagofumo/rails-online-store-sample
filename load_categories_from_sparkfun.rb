2a9024147ba3f383c8478c86049e3c58aad31183
tiagofumo <tiagofumo@gmail.com>
2016-07-25 15:20:18 -0300

    fix bug in load_categories_from_sparkfun.rb

--- load_categories_from_sparkfun.rb
+++ load_categories_from_sparkfun.rb
@@ -9,12 +9,12 @@ categories = JSON.parse open('./categories.json').read
 
 def walk_tree(array, parent_id, categories)
   array.each do |node|
-    id = node['id'].to_s
+    id = node['id']
     category = Category.create name: node['name'],
                                sparkfun_id: id,
                                category_id: parent_id
-    if categories.key? id
-      walk_tree(categories[id], category.id, categories)
+    if categories.key? id.to_s
+      walk_tree(categories[id.to_s], category.id, categories)
     end
   end
 end
