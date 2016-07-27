b1fa9d623a784d9d2e48d3c3aa47d0b189253d9d
tiagofumo <tiagofumo@gmail.com>
2016-07-27 00:37:05 -0300

    Add cost method to ShippingMethod
    The cost method calculates total shipping cost of an array of cart items
    Add weight method to CartItem model to calculate weight multiplied by quantity

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  def weight
+    product.weight * quantity
+  end
 end
--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -1,2 +1,12 @@
 class ShippingMethod < ActiveRecord::Base
+  def cost(cart_items)
+    total_weight = cart_items.inject(0) do |sum, cart_item|
+      sum + cart_item.weight
+    end
+    if total_weight > base_cost_max_weight
+      base_cost + extra_cost_per_gram * (total_weight - base_cost_max_weight)
+    else
+      base_cost
+    end
+  end
 end
