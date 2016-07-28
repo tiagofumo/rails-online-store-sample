7ddb70836ddbe666631a1614d1b5fb5872758681
tiagofumo <tiagofumo@gmail.com>
2016-07-28 05:45:40 -0300

    Add weight and cost methods to OrderItem

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,12 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
+
+  def weight
+    product.weight * quantity
+  end
+
+  def cost
+    price * quantity
+  end
 end
