a936b288b8e2e70181f0a8f7ec06d6d44f85d717
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-06 00:39:38 -0300

    Add tests for category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = categories(:arduino)
+  end
+
+  test "Category should be valid" do
+    assert @category.valid?
+  end
+
+  test "Category without a name should not be valid" do
+    @category.name = ''
+    assert_not @category.valid?
+  end
+
+  test "Category's children should not be empty" do
+    assert_not @category.children.empty?
+  end
 end
