64fed26accb0b93809310237b0d0b08ec66b5361
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-06 00:38:41 -0300

    Add child presence validation for category_relationship

--- app/models/category_relationship.rb
+++ app/models/category_relationship.rb
@@ -1,4 +1,5 @@
 class CategoryRelationship < ActiveRecord::Base
   belongs_to :parent, foreign_key: 'parent_id', class_name: 'Category'
   belongs_to :child, foreign_key: 'child_id', class_name: 'Category'
+  validates :child_id, presence: true
 end
