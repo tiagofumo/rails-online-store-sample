4b28a2cb2b05147a4b84f6875be4021a594a4160
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-05 23:32:31 -0300

    Add tests for products' model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @product = products(:arduino_uno)
+  end
+
+  test 'Product without name should not be valid' do
+    @product.name = ''
+    assert_not @product.valid?
+  end
+
+  test 'Product without description should not be valid' do
+    @product.description = ''
+    assert_not @product.valid?
+  end
+
+  test 'Product without stock should not be valid' do
+    @product.stock = 0
+    assert_not @product.valid?
+  end
+
+  test 'Product without price should not be valid' do
+    @product.price = 0.0
+    assert_not @product.valid?
+  end
 end
