bda5c4661e0e5865b3d39a1562cc6931882f127a
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-05 22:51:31 -0300

    Add validations for essencial product fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
   has_one  :primary_picture, -> { where(default: true) },
            class_name: 'ProductPicture'
+  validates :name,  presence: true, length: { maximum: 80 }
+  validates :price, presence: true, numericality: { greater_than: 0.0 }
+  validates :description,  presence: true
+  validates :stock, presence: true, numericality: { greater_than: 0 }
 end
