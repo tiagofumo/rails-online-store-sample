cc84af0b5c10bb5cdaa865ef23195366a2afc36a
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-07 06:22:24 -0300

    Add test for Category get_tree method

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -17,4 +17,21 @@ class CategoryTest < ActiveSupport::TestCase
   test "Category's children should not be empty" do
     assert_not @category.children.empty?
   end
+
+  test "Category tree nodes should match category relationships" do
+    categories = Category.all.includes(children: :child).to_a.index_by(&:id)
+    tree_to_array(Category.get_tree).each do |node|
+      should_be = categories[node[0]].children.map {|i| i.child_id}.sort
+      assert(node[1].sort == should_be,
+             "Children array of #{node[0]}:#{categories[node[0]].name}, " \
+             "should be #{should_be} but was #{node[1]}")
+    end
+  end
+
+  def tree_to_array(tree)
+    ary_of_arys = tree.map do |node|
+      [[ node[0].id, node[1].map{ |i| i[0].id }.sort ]] + tree_to_array(node[1])
+    end
+    ary_of_arys.inject([]) { |flat_ary, ary| flat_ary + ary }
+  end
 end
