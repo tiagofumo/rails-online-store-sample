e43669dd804ddb3f1b063a2bd117952263aa35ff
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-08 06:51:41 -0300

    Add test for Address country_name method

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should match country name" do
+    assert_equal 'Brazil', Address.new(country: 'BR').country_name
+    assert_equal 'United States', Address.new(country: 'US').country_name
+    assert_equal 'Poland', Address.new(country: 'PL').country_name
+    assert_equal 'Germany', Address.new(country: 'DE').country_name
+  end
 end
