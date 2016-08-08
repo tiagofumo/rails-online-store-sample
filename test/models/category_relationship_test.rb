69a3d3d355ff5df69ed2b60b57016a2f14e9af00
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-08 09:47:40 -0300

    Add test for CategoryRelationship model

--- test/models/category_relationship_test.rb
+++ test/models/category_relationship_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CategoryRelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "CategoryRelationship without child_id should not be valid" do
+    assert_not CategoryRelationship.new(child_id: nil).valid?
+  end
 end
