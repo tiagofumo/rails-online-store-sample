fca7588828bbcd6f307faac7ad6f98ace33d0367
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-13 07:52:47 -0300

    Add validations for Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :user
+  validates :user,  presence: true
+  validates :title, :receiver, :street, :country, :state,
+            :city, :postal_code, presence: true
 
   def country_name
     country_iso = ISO3166::Country[country]
