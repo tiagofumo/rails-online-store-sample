978866c58d6d6a6c8a6d5a4e5ff5c59e958fc2b3
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-13 10:07:57 -0300

    Refactor Address model spec with sholda-matchers

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,27 +1,23 @@
 require "rails_helper"
 
 RSpec.describe Address, :type => :model do
-  it "matches country name" do
-    expect(Address.new(country: 'BR').country_name).to eq('Brazil')
-    expect(Address.new(country: 'US').country_name).to eq('United States')
-    expect(Address.new(country: 'PL').country_name).to eq('Poland')
-    expect(Address.new(country: 'DE').country_name).to eq('Germany')
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:receiver) }
+    it { is_expected.to validate_presence_of(:street) }
+    it { is_expected.to validate_presence_of(:country) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:postal_code) }
+    it { is_expected.to validate_presence_of(:user) }
   end
 
-  it "is valid" do
-    address = FactoryGirl.build(:address)
-    expect(address).to be_valid
-  end
-
-  it "is not valid" do
-    address = FactoryGirl.build(:address)
-    [ :title, :receiver, :street, :country, :state,
-        :city, :postal_code ].each do |field|
-      address_clone = address.clone
-      address_clone.send("#{field}=", "")
-      expect(address_clone).to_not be_valid
+  context 'country_name' do
+    it "matches country name" do
+      expect(Address.new(country: 'BR').country_name).to eq('Brazil')
+      expect(Address.new(country: 'US').country_name).to eq('United States')
+      expect(Address.new(country: 'PL').country_name).to eq('Poland')
+      expect(Address.new(country: 'DE').country_name).to eq('Germany')
     end
-    address.user = nil
-    expect(address).to_not be_valid
   end
 end
