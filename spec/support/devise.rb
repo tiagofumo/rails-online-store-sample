7164e3980ba2a80de7ee0009c624f81e6100af85
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-13 07:50:23 -0300

    Add custom helpers for devise

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,5 +1,6 @@
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include DeviseHelpers, :type => :controller
   config.include Warden::Test::Helpers
 
   config.before :suite do
--- spec/support/helpers/controller_macros.rb
+++ spec/support/helpers/controller_macros.rb
@@ -2,8 +2,8 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
-      sign_in user
+      @user = FactoryGirl.create(:user)
+      sign_in @user
     end
   end
 end
