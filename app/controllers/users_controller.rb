f6d0ce13a899597563749ec90c90200e6b164d92
Tiago Mendonça <tiagofumo@gmail.com>
2016-08-14 06:09:00 -0300

    Fix update password not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,8 +61,9 @@ class UsersController < ApplicationController
   end
 
   def update_password
-    user_params = params.require(:user).permit(:password, :password_confirmation)
-    if @current_user.valid_password? params.require(:user)[:old_password]
+    user_params = params.require(:user).permit(:current_password, :password,
+                                               :password_confirmation)
+    if @current_user.valid_password? user_params[:current_password]
       if @current_user.update_with_password user_params
         sign_in @current_user, bypass: true
         redirect_to :account
@@ -80,6 +81,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :password, :email)
+      params.require(:user).permit(:name, :password,
+                                   :password_confirmation, :email)
     end
 end
